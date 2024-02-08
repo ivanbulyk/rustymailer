5726ddaf775804803b7f98e2c36a927e31d1e580
Ivan Bulyk <stackyogi@gmail.com>
2024-02-08 16:27:07 +0100

    scaffolded initial project structure. added /healthcheck endpoint. added integration test for it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hi I'm an Email Newsletter System!");
+use rustymailer::run;
+use std::net::TcpListener;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
