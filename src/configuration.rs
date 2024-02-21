1b9f91cb7659a527545258233db66c07b3fdda93
Ivan Bulyk <stackyogi@gmail.com>
2024-02-21 19:03:27 +0100

    reduced spec.yaml a bit, in order to try different dbaas

--- src/configuration.rs
+++ src/configuration.rs
@@ -10,18 +10,18 @@ pub struct Settings {
 #[derive(serde::Deserialize)]
 pub struct ApplicationSettings {
     #[serde(deserialize_with = "deserialize_number_from_string")]
-    pub port: u16,
     pub host: String,
+    pub port: u16,
 }
 
 #[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
-    pub username: String,
-    pub password: String,
+    pub database_name: String,
+    pub host: String,
     #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
-    pub host: String,
-    pub database_name: String,
+    pub username: String,
+    pub password: String,
     pub require_ssl: bool,
 }
 
